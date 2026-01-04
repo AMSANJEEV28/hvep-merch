0438f797ede12ca96ca57af9edbdc050fc3e76ad
AMSANJEEV28 <amsanjeev28@gmail.com>
2026-01-05 02:01:32 +0530

    UPDATE

--- config/settings.py
+++ config/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # 👈 THIS enables base.html
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
